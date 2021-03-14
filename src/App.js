a959ac862d72c2047ac3a0cb63a535ba2bb8da55
ilknurmert <m.ilknur11@gmail.com>
2021-03-14 23:51:00 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss'
+import Search from './components/Search/Search';
 
-function App() {
+export default function App() {
+
+  const [city , setCity] = useState("");
+  const [search , setSearch] = useState("");
+  const [weatherInfo , setweatherInfo] = useState("");
+
+  useEffect(() => {
+    fetch(`http://api.weatherapi.com/v1/forecast.json?key=7620cc8104e54349b02200850211103&q=${city}&days=3&aqi=no&alerts=no`)
+    .then((res) => res.json())
+    .then((data) => {
+      setweatherInfo(data)
+    })
+  })
+
+  function handlerOnChange(event) {
+    setSearch(event.target.value)
+  }
+
+  function handlerOnKeyDown(event) {
+    if (event.keyCode === 13 && search.length>1) {
+      setCity(event.target.value)
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Search
+      onChange = {event => handlerOnChange(event)}
+      onKeyDown = {event => handlerOnKeyDown(event)}
+      onBlur = {() => setCity(search)}
+    >
+    </Search>
+    
+    {weatherInfo.current && weatherInfo.forecast.forecastday.map((item) => {
+      return(
+        <>
+        <div>{item.date}</div>
+        <img src={`${item.day.condition.icon}`} alt="icon"></img>
+        <div>{item.day.condition.text}</div>
+        <div>{`${item.day.maxtemp_c}° / ${item.day.maxtemp_c}°`}</div>
+        </>
+      )
+    })}
+    </>
+  )
 }
-
-export default App;
---
+++ src/components/Search/Search.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Search.scss'
+
+export default function Search( {onChange, onKeyDown, onBlur} ) {
+
+
+return (
+    <div class = "search-wrapper">
+        <label htmlFor="search">
+            <input 
+                id="search"
+                type="text"
+                name="search"
+                placeholder="Enter a city"
+                onChange={onChange}
+                onKeyDown={onKeyDown}
+                 onBlur={onBlur}
+                 />
+        </label>
+    </div>
+    )
+}
